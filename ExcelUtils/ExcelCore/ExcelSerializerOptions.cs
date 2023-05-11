d44f25acc0945b7d9c2478f88b9ac376d0af89c5
鸣 <lyj90601991@163.com>
2023-05-12 00:39:56 +0800

    excel

--- ExcelUtils/ExcelCore/ExcelSerializerOptions.cs
+++ ExcelUtils/ExcelCore/ExcelSerializerOptions.cs
@@ -5,5 +5,28 @@ namespace ExcelUtils.ExcelCore;
 //待定：后面根据功能再重构
 internal class ExcelSerializerOptions
 {
-    private Func<PropertyInfo, bool>? FieldFilter;
+    private Func<Type, InfoWrapper<PropertyTypeInfo>>? _propertySelector;
+
+    public Func<Type, InfoWrapper<PropertyTypeInfo>> PropertySelector => _propertySelector ?? DefaultPropertySelector.GetTypeInfo;
+
+    public void SetProperty(Func<Type, InfoWrapper<PropertyTypeInfo>> selector)
+    {
+        _propertySelector = selector;
+    }
+}
+internal static class DefaultPropertySelector
+{
+    internal static InfoWrapper<PropertyTypeInfo> GetTypeInfo(Type type)
+    {
+        var result= new InfoWrapper<PropertyTypeInfo>();
+        foreach (var property in type.GetProperties())
+        {
+            var attribute = property.GetCustomAttribute<DisplayAttribute>();
+            if (attribute != null)
+            {
+                result.Add(new DefaultPropertyInfo(property,attribute));
+            }
+        }
+        return result;
+    }
 }
