adbcc90a601d4e0e5258f5d31ca915b180180c62
mingrenliu <lyj90601991@163.com>
2023-07-04 20:05:36 +0800

    test

--- Analyzers/ServiceAnalyzer.Test/ServiceAnalyzerUnitTests.cs
+++ Analyzers/ServiceAnalyzer.Test/ServiceAnalyzerUnitTests.cs
@@ -1,17 +1,36 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft;
+using ServiceAnalyzer.Diagnostics;
+using System.Threading.Tasks;
 using VerifyCS = ServiceAnalyzer.Test.CSharpCodeFixVerifier<
     ServiceAnalyzer.ServiceDiagnostic,
     ServiceAnalyzer.ServiceCodeFixProvider>;
 
-namespace MakeConst.Test
+namespace ServiceAnalyzer.Test
 {
     [TestClass]
-    public class MakeConstUnitTest
+    public class ServiceAnalyzerUnitTest
     {
         [TestMethod]
         public async Task LocalIntCouldBeConstant_Diagnostic()
         {
-            await VerifyCS.VerifyCodeFixAsync(@"using System;
+            var sourceCode = @"using System;
+
+namespace ServiceAnalyzer.Test
+{
+    public class {|#1:ConfigService|} : IConfigService
+    {
+        public void Display(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public interface {|#0:IConfigService|}
+    {
+    }
+}
+";
+            var fixedCode= @"using System;
 
 namespace ServiceAnalyzer.Test
 {
@@ -27,23 +46,12 @@ namespace ServiceAnalyzer.Test
     {
     }
 }
-", @"using System;
+";
+            var expected = VerifyCS.Diagnostic(ServiceHintDiagnostic.DiagnosticId).WithLocation(0).WithLocation(1).WithArguments("ConfigService")
+                .WithSeverity(Microsoft.CodeAnalysis.DiagnosticSeverity.Error);
+            
+            await VerifyCS.VerifyCodeFixAsync(sourceCode,expected, sourceCode);
 
-namespace ServiceAnalyzer.Test
-{
-    public class ConfigService : IConfigService
-    {
-        public void Display(string name)
-        {
-            Console.WriteLine(name);
-        }
-    }
-
-    public interface IConfigService
-    {
-        void Display(string name);
-    }
-}");
         }
     }
 }
