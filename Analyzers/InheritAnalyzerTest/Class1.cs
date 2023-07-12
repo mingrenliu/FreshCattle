3a3d64824a7952f6c59bc57ef1865c6915dae232
mingrenliu <lyj90601991@163.com>
2023-07-12 22:24:13 +0800

    analyzer

---
+++ Analyzers/InheritAnalyzerTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace InheritAnalyzerTest
+{
+    public class Class1
+    {
+        private readonly ITest test; 
+        public string Getstring(string name,IEnumerable<string> list)
+        {
+            return test.Getstring(name,list);
+        }
+        //public string
+    }
+
+    public interface ITest
+    {
+        string Getstring(string name, IEnumerable<string> list);
+    }
+}
