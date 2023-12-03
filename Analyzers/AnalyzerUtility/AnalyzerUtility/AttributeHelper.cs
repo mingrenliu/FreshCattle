2c14b63398c4c710a9187b4be3303964dc9df26c
mingrenliu <lyj90601991@163.com>
2023-12-03 23:20:16 +0800

    fed:create analyzer common utility

---
+++ Analyzers/AnalyzerUtility/AnalyzerUtility/AttributeHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AnalyzerUtility;
+
+public static class AttributeHelper
+{
+    public static bool HaveAttribute(this SyntaxList<AttributeListSyntax> attributes, string name)
+    {
+        return attributes.SelectMany(x => x.Attributes).Any(x => x.Name.ToString() == name);
+    }
+
+    public static bool TryGetAttribute(this SyntaxList<AttributeListSyntax> attributes, string name, out AttributeSyntax? attribute)
+    {
+        attribute = attributes.SelectMany(x => x.Attributes).FirstOrDefault(x => x.Name.ToString() == name);
+        return attribute != null;
+    }
+
+    public static bool HaveAttribute(this SyntaxList<AttributeListSyntax> attributes, IEnumerable<string> names)
+    {
+        return attributes.SelectMany(x => x.Attributes).FirstOrDefault(x => names.Contains(x.Name.ToString())) != null;
+    }
+
+    public static bool TryGetAttributeFirstArgString(this AttributeSyntax node, out string content)
+    {
+        content = string.Empty;
+        var first = node.ArgumentList?.Arguments.FirstOrDefault();
+        if (first != null && first.Expression is LiteralExpressionSyntax literal)
+        {
+            content = literal.Token.ValueText;
+            return !string.IsNullOrWhiteSpace(content);
+        }
+        return false;
+    }
+}
---
+++ Analyzers/AnalyzerUtility/AnalyzerUtility/XmlDocumentHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AnalyzerUtility;
+
+public static class XmlDocumentHelper
+{
+    public static bool TryGetXmlDocument(this SyntaxNode node, out DocumentationCommentTriviaSyntax? doc)
+    {
+        doc = node.GetLeadingTrivia().Select(i => i.GetStructure()).OfType<DocumentationCommentTriviaSyntax>().FirstOrDefault();
+        return doc != null;
+    }
+
+    public static bool TryGetComment(this DocumentationCommentTriviaSyntax doc, out string comment)
+    {
+        comment = string.Empty;
+        foreach (var node in doc.DescendantNodes().OfType<XmlElementSyntax>())
+        {
+            if (node.StartTag.Name.ToString() == "summary")
+            {
+                foreach (var text in node.Content.OfType<XmlTextSyntax>())
+                {
+                    comment = string.Join(" ", text.TextTokens.Where(x => x.IsKind(SyntaxKind.XmlTextLiteralToken)).Select(x => x.ValueText));
+                    return !string.IsNullOrWhiteSpace(comment);
+                }
+            }
+        }
+        return false;
+    }
+}
